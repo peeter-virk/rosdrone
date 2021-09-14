7a57b40c8a9a6fb4b4b8310023cf7ae5bbf58026
peeter-virk <32205703+peeter-virk@users.noreply.github.com>
2021-09-14 12:59:32 +0300

    Add files via upload

---
+++ tsrot1.py
@@ -0,0 +1,14 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+def talker():
+    pub = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+    rospy.init_node('turtlesim_motion_pose', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        vel_msg = Twist()
+        vel_msg.angular.z = 1
+        vel_msg.linear.x = 1
+        pub.publish(vel_msg)
+        rate.sleep()
+talker()
